b8a9bfccf1fc87fadb014288b593af86f6786e8f
Noureldin-Ahmed-FullStack <noureldin2662002@gmail.com>
2025-09-05 00:18:26 +0300

    w

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,51 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+        // 👇 Do NOT serve index.html for these URLs
+        navigateFallbackDenylist: [
+          /sitemap\.xml$/,
+          /robots\.txt$/
+        ],
+      },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: "Julie's Art Gallery",
+        short_name: "Julie's Art Gallery",
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ],
+      },
+    })
+  ],
 })
